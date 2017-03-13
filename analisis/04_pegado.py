6a4e2f26b9875a1c7a87a76a30614ba8c15ff047
CLM <charlielm1015@hotmail.com>
2017-03-13 12:15:38 -0600

    añadimos archivo '4_pegado.py para pegar archivos chiquitos

--- analisis/04_pegado.py
+++ analisis/04_pegado.py
@@ -4,6 +4,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import datetime
+import glob
+import os
 import pymysql.cursors
 
+# prueba: path = "."
+path1 = "/opt/aws-ml/.data/"
+path2 = "8d06f930-4016-4911-8c12-2cc0f92a5b78"
+path = path1 + path2
 
+# Leemos archivos y ponemos en lista
+all_files = glob.glob(os.path.join(path, "*.csv"))
+# Convertios a dataframe
+df_files = (pd.read_csv(file) for file in all_files)
+# Concatenamos archivos con Pandas
+concat_file = pd.concat(df_files, ignore_index = True)
+# Exportamos a archivo
+concat_file.to_csv("full_f.csv", index = False)
+# Borramos archivos chiquitos
+command = "rm " + " ".join(map(str, all_files))
+os.system(command)
