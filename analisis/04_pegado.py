bacd7b16743ea0d91516b0b444273a78ab369c6a
CLM <charlielm1015@hotmail.com>
2017-03-13 13:22:23 -0600

    se añadio ruta d directorio en AWS

--- analisis/04_pegado.py
+++ analisis/04_pegado.py
@@ -10,7 +10,9 @@ import os
 # Ruta a dir .data
 path1 = "/opt/aws-ml/.data/worker/"
 # Id del proceso actual
-path2 = "8d06f930-4016-4911-8c12-2cc0f92a5b78"
+# path2 = "8d06f930-4016-4911-8c12-2cc0f92a5b78"
+# Ruta en AWS
+path2 = "5318f322-6e90-4952-b62f-19f5e21c3720"
 path = path1 + path2
 
 command = "cd " + path
